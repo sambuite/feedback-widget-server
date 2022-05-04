ffd45ca63e26ed04ec83af1eed9c710354f515bf
sambuite <murilosambuite@gmail.com>
2022-05-04 09:47:35 -0300

    add nodemailer to send emails

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '27a2871292eba5',
+    pass: '3204275357a198',
+  },
+});
 
 app.post('/feedback', async (req, res) => {
   const { comment, type, screenshot } = req.body;
@@ -14,6 +24,16 @@ app.post('/feedback', async (req, res) => {
       type,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Murilo Sambuite <murilosambuite@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<p>Tipo de feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+    ].join('\n'),
   });
 
   return res.status(201).json({ data: feedback });
